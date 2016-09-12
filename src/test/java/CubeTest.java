d75fb361d2e4bc93d00695e7306f1b92d68b431f
Courtney Phillips <courtney@epicodus.com>
2016-09-12 14:22:20 -0700

    Write test asserting surfaceArea() method can accurately determine the surface area of a Cube object.

--- src/test/java/CubeTest.java
+++ src/test/java/CubeTest.java
@@ -11,10 +11,24 @@ public class CubeTest {
   }
 
   @Test
-  public void newCube_savesRectangleInformation() {
+  public void newCube_savesRectangleInformation_Rectangle() {
     Rectangle testRectangle = new Rectangle(30, 30);
     Cube testCube = new Cube(testRectangle);
     assertEquals(testRectangle, testCube.getFace());
+  }
+
+  @Test
+  public void volume_determinesTheVolumeOfTheCube_27000() {
+    Rectangle testRectangle = new Rectangle(30, 30);
+    Cube testCube = new Cube(testRectangle);
+    assertEquals(27000, testCube.volume());
+  }
+
+  @Test
+  public void surfaceArea_determinesTheSurfaceAreaOfACube_5400() {
+    Rectangle testRectangle = new Rectangle(30, 30);
+    Cube testCube = new Cube(testRectangle);
+    assertEquals(5400, testCube.surfaceArea());
   }
 
 }
