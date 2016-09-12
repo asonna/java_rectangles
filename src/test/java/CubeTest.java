68346a58bdd766bebe074b4e508db165daec6606
Courtney Phillips <courtney@epicodus.com>
2016-09-12 13:56:03 -0700

    Write test asserting program can succesfully intansitate Cube objects.

--- src/test/java/CubeTest.java
+++ src/test/java/CubeTest.java
@@ -3,4 +3,11 @@ import static org.junit.Assert.*;
 
 public class CubeTest {
 
+  @Test
+  public void newCube_instantiatesCorrectly() {
+    Rectangle testRectangle = new Rectangle(30, 30);
+    Rectangle testCube = new Cube(testRectangle);
+    assertEquals(true, testCube instanceof Cube);
+  }
+
 }
